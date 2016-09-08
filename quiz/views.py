d351e512165f812cc54a235f385e1aff20adf164
Siri Berggren <siri.matsdotter.berggren@gmail.com>
2016-09-08 11:02:32 +0200

    Imported test data in views

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+quizzes = [
+	{
+		"quiz_number": 1,
+   		"name": "Klassiska böcker",
+	   	"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+   	   	"name": "Största 1slagen",
+	   	"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+   	    	"name": "Världens mest kända hackare",
+	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+]
+
+
 # Create your views here.
 def start(request):
 	return render(request, "quiz/start.html")
