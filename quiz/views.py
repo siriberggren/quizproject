99d578c3e444334f08e635fe506e9b007d090784
Siri Berggren <siri.matsdotter.berggren@gmail.com>
2016-09-08 11:11:32 +0200

    changed definition of quiz

--- quiz/views.py
+++ quiz/views.py
@@ -24,7 +24,12 @@ def start(request):
 	return render(request, "quiz/start.html", context)
 
 def quiz(request):
-	return render(request, "quiz/quiz.html")
+	def quiz(request, quiz_number):
+	context = {
+		"quiz": quizzes[quiz_number - 1],
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz/quiz.html", context)
 
 def question(request):
 	return render(request, "quiz/question.html")
