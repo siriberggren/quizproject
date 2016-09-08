e603e8a67117b5c79f74048bb44bf40bc9a0c9ab
Siri Berggren <siri.matsdotter.berggren@gmail.com>
2016-09-08 10:47:57 +0200

    Ändrade namn på view för att matcha mina filer

--- quiz/views.py
+++ quiz/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def startpage(request):
-return render(request, "quiz/startpage.html")
+return render(request, "quiz/start.html")
 
 def quiz(request):
 return render(request, "quiz/quiz.html")
@@ -11,4 +11,4 @@ def question(request):
 return render(request, "quiz/question.html")
 
 def completed(request):
-return render(request, "quiz/completed.html")
+return render(request, "quiz/results.html")
